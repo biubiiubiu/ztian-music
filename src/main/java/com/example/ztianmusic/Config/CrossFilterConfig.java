426b6141db5cf97e50a2d8b1b606355b5e2a089d
zt <3141616128@qq.com>
2022-03-12 11:20:04 +0800

    add: FilterConfig

---
+++ src/main/java/com/example/ztianmusic/Config/CrossFilterConfig.java
@@ -0,0 +1,33 @@
+package com.example.ztianmusic.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * description: 跨域过滤器配置
+ *
+ * @author: zhangtian
+ * @since: 2022-03-12 10:53
+ */
+@Configuration
+public class CrossFilterConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        // 允许所有域名进行跨域调用
+        config.addAllowedOriginPattern("*");
+        // 允许跨域发送Cookie
+        config.setAllowCredentials(true);
+        // 放行全部原始头信息
+        config.addAllowedHeader("*");
+        // 允许所有请求方法跨域调用
+        config.addAllowedMethod("*");
+        config.addExposedHeader("*");
+        UrlBasedCorsConfigurationSource urlSource = new UrlBasedCorsConfigurationSource();
+        urlSource.registerCorsConfiguration("/**", config);
+        return new CorsFilter(urlSource);
+    }
+}
